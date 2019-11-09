f2cb4590ba2360514bfba4c44b2e7eaca40d605f
John Taylor <vandammit091281@gmail.com>
2019-11-09 12:26:29 +0000

    Home layout fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-sm"></div>
-
             <div class="col-sm">
                 <h1>GymApp</h1>
                 <p>Login to access your profile.</p>
             </div>
-            
-            <div class="col-sm"></div>
         </div>
-        
     </div>
 @endsection
         
