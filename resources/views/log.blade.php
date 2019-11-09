938a119603e1019da4a0764ac2c7b822d5725996
John Taylor <vandammit091281@gmail.com>
2019-11-09 12:24:54 +0000

    Added log/profile basic views/routes

---
+++ resources/views/log.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+            <h1>Log of Workouts</h1>
+            
+            </div>
+        </div>
+        
+    </div>
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+            <h1>Profile</h1>
+            
+            </div>
+        </div>
+        
+    </div>
+@endsection
