d3e09a88773426e56d7db0a70fd4d0797420d731
John Taylor <johntaylor@Johns-iMac.local>
2019-12-19 16:25:27 +0000

    Radio select on Workout fixed

--- resources/views/workouts/create.blade.php
+++ resources/views/workouts/create.blade.php
@@ -16,27 +16,65 @@
 
                     <p>Choose an existing WOD or manually log a workout.</p>
 
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="workoutradio" id="choice-wod" checked>
-                        <label class="form-check-label" for="choice-wod">
-                          Search for an existing WOD
-                        </label>
-                    </div>
+                    <form method="POST" action="#">
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="workoutradio" id="choice-wod" checked>
+                            <label class="form-check-label" for="choice-wod">
+                            Search for an existing WOD
+                            </label>
 
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="workoutradio" id="choice-manual">
-                        <label class="form-check-label" for="choice-manual">
-                          Manual Entry
-                        </label>
-                    </div>
+                            <div class="reveal-if-active">
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="wodsearch" id="wodsearch" placeholder="WOD Description">
+                                </div>
+                            </div>
+                        </div>
 
-                    <div class="form-group reveal-if-active">
-                        <input class="form-control" type="text" name="wodsearch" id="wodsearch" placeholder="WOD Description">
-                    </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="workoutradio" id="choice-manual">
+                            <label class="form-check-label" for="choice-manual">
+                            Manual Entry
+                            </label>
+                        </div>
+
+                        <input type="submit" name="continue" id="continue" class="btn btn-primary" value="Continue" />
+
+                    </form>
 
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+var FormStuff = {
+
+    init: function() {
+      this.applyConditionalRequired();
+      this.bindUIActions();
+    },
+
+    bindUIActions: function() {
+      $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+    },
+
+    applyConditionalRequired: function() {
+
+      $(".require-if-active").each(function() {
+        var el = $(this);
+        if ($(el.data("require-pair")).is(":checked")) {
+          el.prop("required", true);
+        } else {
+          el.prop("required", false);
+        }
+      });
+
+    }
+
+  };
+
+  FormStuff.init();
+</script>
+
 @endsection
