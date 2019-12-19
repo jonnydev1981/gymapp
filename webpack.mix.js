1f471225ba198bd2ab8b3f7bc88d591cff3aa746
John Taylor <johntaylor@Johns-iMac.local>
2019-12-19 14:54:25 +0000

    Fixed CSS/JS Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js');
-mix.copy('node_modules/jquery-ui-dist/jquery-ui.min.js', 'public/js');
-mix.copy('node_modules/jquery-ui-dist/jquery-ui.min.css', 'public/css');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -18,4 +14,4 @@ mix.copy('node_modules/jquery-ui-dist/jquery-ui.min.css', 'public/css');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-   
+
