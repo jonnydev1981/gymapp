f7e78e109a7f1b373ba5720e26359151484ee5f4
John Taylor <vandammit091281@gmail.com>
2019-12-26 09:49:48 +0000

    Modal play button added for exercise

--- resources/views/exercises/index.blade.php
+++ resources/views/exercises/index.blade.php
@@ -1,6 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+
+<script>
+    $(document).ready(function() {
+
+        // Gets the video src from the data-src on each button
+        
+        var $videoSrc;  
+        $('.video-btn').click(function() {
+            $videoSrc = $(this).data( "src" );
+        });
+        console.log($videoSrc);
+          
+        // when the modal is opened autoplay it  
+        $('#myModal').on('shown.bs.modal', function (e) {
+            
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+        })
+        
+        // stop playing the youtube video when I close the modal
+        $('#myModal').on('hide.bs.modal', function (e) {
+            // a poor man's stop video
+            $("#video").attr('src',$videoSrc); 
+        }) 
+
+        // document ready  
+        });            
+</script>
+
 <div class="container">
     <div class="row justify-content-center">
         @if (session('status'))
@@ -21,10 +50,43 @@
             <tbody>
             @foreach ($exercises as $exercise)
                 <tr>
-                    <th scope="row">{{ $exercise->name }}</th>
-                    <td>{{ $exercise->description }}</td>
-                    <td>{{ $exercise->url }}</td>
-                    <td><a type="button" class="btn btn-primary" href="{{ route('exercise.edit', $exercise->id) }}">Edit</button></td>
+                    <th scope="row">
+                        {{ $exercise->name }}
+                    </th>
+
+                    <td>
+                        {{ $exercise->description }}
+                    </td>
+                    
+                    <td>
+                        @isset($exercise->url)
+                            <button type="button" class="btn btn-primary video-btn" data-toggle="modal" data-src="{{ $exercise->url }}" data-target="#myModal">
+                                Play
+                            </button>
+
+                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>        
+                                            
+                                            <div class="embed-responsive embed-responsive-16by9">
+                                                <iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> 
+                            &nbsp;
+                            <a href="{{ $exercise->url }}">{{ $exercise->url }}</a>
+                        @endisset
+                    </td>
+
+                    <td>
+                        <a type="button" class="btn btn-primary" href="{{ route('exercise.edit', $exercise->id) }}">Edit</button>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
@@ -33,4 +95,5 @@
             {{ $exercises->links() }}
     </div>
 </div>
+
 @endsection
