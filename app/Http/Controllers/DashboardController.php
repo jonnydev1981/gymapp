960d7f9452cc57ccf5acb474f6728cefc277cbf0
John Taylor <vandammit091281@gmail.com>
2019-12-27 09:16:47 +0000

    One rep max beginning

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Workout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
+        //$oneRepMaxes = Workout::where('user_id', Auth::id())->workoutlines()->get();
+        
+        //return view ('dashboards.index')->with('onerepmaxes', $oneRepMaxes);
         return view ('dashboards.index');
     }
 }
--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -13,7 +13,7 @@
     </div>
 
     <div class="row justify-content-center">
-
+        
     </div>
 </div>
 @endsection
