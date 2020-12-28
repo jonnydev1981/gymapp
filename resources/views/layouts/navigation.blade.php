15d5a7e457edf7db08181877b9f079893dd3c3ba
John Taylor <vandammit091281@gmail.com>
2020-12-28 10:43:48 +0000

    Beginning to link in workouts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('workout.index')" :active="request()->routeIs('workout.index')">
+                        {{ __('Workouts') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -65,6 +68,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('workout.index')" :active="request()->routeIs('workout.index')">
+                {{ __('Workouts') }}
             </x-responsive-nav-link>
         </div>
 
--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -1,7 +1,16 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Workouts') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="container">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+    
+
     <div class="row justify-content-center">
 
         @if ($errors->any())
@@ -50,5 +59,10 @@
             @endisset
         </div>
     </div>
+
 </div>
-@endsection
+</div>
+</div>
+</div>            
+
+</x-app-layout>
